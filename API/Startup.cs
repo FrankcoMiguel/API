db4329b5c5f3c17936c911ad4afc955d92a646b4
Frank Orozco <20175332@itla.edu.do>
2019-07-09 00:30:38 -0800

    connection string created

--- API/Startup.cs
+++ API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Persistence;
 
 namespace API
 {
@@ -25,6 +26,9 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var connection = ?;
+            services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(connection));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
