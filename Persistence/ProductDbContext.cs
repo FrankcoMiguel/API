eed4dbb1ac9c19443f3143a05d9ac0a33dbf1e55
Frank Orozco <20175332@itla.edu.do>
2019-07-09 00:18:19 -0800

    product dbcontext added

---
+++ Persistence/ProductDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Persistence
+{
+   public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+    }
+}
