74911e63b03a1ac56dbe80c76fcdb488f11d5f93
Frank Orozco <20175332@itla.edu.do>
2019-07-09 00:37:44 -0800

    connection strings bug fixed

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -27,7 +28,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            var connection = ?;
+            var connection = Configuration.GetConnectionString("Dev");
             services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(connection));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
--- Model/Product.cs
+++ Model/Product.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace Model
 {
-    class Product
+    public class Product
     {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public int Quantity { get; set; }
     }
 }
